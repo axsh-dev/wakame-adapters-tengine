5c6e3b563b143aa9c8bef67cfc1f6626497db590
Metallion <akieckens@axsh.net>
2011-11-09 17:41:59 +0900

    Added configurable rspec autotesting

--- test/spec/real_api_spec.rb
+++ test/spec/real_api_spec.rb
@@ -1,97 +1,115 @@
 load_path = "#{File.expand_path(File.dirname(__FILE__))}/../.."
 $LOAD_PATH.unshift(load_path) unless $LOAD_PATH.include?(load_path)
 
-require 'tama'
 require 'spec_helper'
+require 'tama'
 
-# Api parameters
-Account = "a-shpoolxx"
+require 'yaml'
+config = YAML.load_file File.expand_path('../config.yml', __FILE__)
 
-Ec2_host = "192.168.2.22"
-Ec2_port = "9005"
-Ec2_protocol = "http"
+## Api parameters
+#Account = "a-shpoolxx"
 
-Wakame_host = "127.0.0.1"
-Wakame_port = "9001"
-Wakame_protocol = "http"
+#Ec2_host = "192.168.2.22"
+#Ec2_port = "9005"
+#Ec2_protocol = "http"
 
-# Run_instance parameters
-Image_id = 'wmi-lucid5'
-Min = 3
-Max = 3
-Security_Groups = ['default']
-SSH_Key = 'demopair'
-User_Data = 'joske'
-Addressing_Type = 'public'
-Instance_Type = 'is-demospec'
-HOST = 'hp-demo1'
+#Wakame_host = "127.0.0.1"
+#Wakame_port = "9001"
+#Wakame_protocol = "http"
+
+## Run_instance parameters
+#Image_id = 'wmi-lucid5'
+#Min = 3
+#Max = 3
+#Security_Groups = {'ng-demofgr'=>'default','ng-82bn95ou'=>'joske','ng-4udfccup'=>'jefke'}#['default']
+#SSH_Key = 'demopair'
+#User_Data = 'joske'
+#Addressing_Type = 'public'
+#Instance_Type = 'is-demospec'
+#HOST = 'hp-demo1'
 
 # Initialize some global variables we'll be using later
-$res = []
-$inst_ids = []
 
-describe "TamaController ( Tama.new(#{Account},#{Ec2_host},#{Ec2_port},#{Ec2_protocol},#{Wakame_host},#{Wakame_port},#{Wakame_protocol}) )" do
-  before do
-    # Initialize our controller
-    @tama = Tama::Tama.new(Account,Ec2_host,Ec2_port,Ec2_protocol,Wakame_host,Wakame_port,Wakame_protocol)
+#( Tama.new(#{Account},#{Ec2_host},#{Ec2_port},#{Ec2_protocol},#{Wakame_host},#{Wakame_port},#{Wakame_protocol}) )
+describe "TamaController" do
+  tama = Tama::Tama.new(config["Account"],config["Ec2_host"],config["Ec2_port"],config["Ec2_protocol"],config["Wakame_host"],config["Wakame_port"],config["Wakame_protocol"])
+  res = tama.run_instances(config["Image_id"],config["Min"],config["Max"],config["Security_Groups"].values,config["SSH_Key"], config["User_Data"], config["Addressing_Type"], config["Instance_Type"], nil, nil, config["HOST"])
+  
+  describe "run_instances" do
+    it_should_behave_like "run_instances", res, config
+    it_should_behave_like "run_instances return values", res, config
   end
   
-  it "Should start a number of instances between #{Min} and #{Max}" do
-    # Try to start instances
-    $res = @tama.run_instances(Image_id,Min,Max,Security_Groups,SSH_Key, User_Data, Addressing_Type, Instance_Type, nil, nil, HOST)
-    
-    # Check if the correct amount of instances where started
-    $res.length.should >= Min
-    $res.length.should <= Max
-    
-    # Prepare the instance ids to be used in further examples
-    $inst_ids = $res.map{|i| i[:aws_instance_id]}; 
+  describe "describe_instances" do
+    @desc_res = tama.describe_instances(res.map{|i| i[:aws_instance_id]})
+    it_should_behave_like "run_instances return values", @desc_res, config
+    it_should_behave_like "describe_instances return values", @desc_res, config
   end
   
-  it "Should describe all instances" do
-    insts = @tama.describe_instances
-    insts.empty?.should_not be_true
-    insts.nil?.should_not be_true
-    insts.length.should >= $res.length
+  describe "terminate_instances" do
+    @uuids = res.map{|i| i[:aws_instance_id]}
+    it_should_behave_like "terminate_instances", tama, @uuids
   end
+  
+  #before(:all) do
+    ## Initialize our controller
+    ##@tama = Tama::Tama.new(Account,Ec2_host,Ec2_port,Ec2_protocol,Wakame_host,Wakame_port,Wakame_protocol)
+    #@tama = tama
+    ## Start our test instances
+    ##$res = @tama.run_instances(Image_id,Min,Max,Security_Groups,SSH_Key, User_Data, Addressing_Type, Instance_Type, nil, nil, HOST)
+    #$inst_ids = []
+    #$desc_res = []
+  #end
+  
+  #it "Should describe all instances" do
+    #insts = @tama.describe_instances
+    #insts.empty?.should_not be_true
+    #insts.nil?.should_not be_true
+    #insts.length.should >= $res.length
+  #end
 
-  it "Should describe instances #{$inst_ids.join(",")}" do
-    insts = @tama.describe_instances($inst_ids)
+  #it "Should describe instances #{$inst_ids.join(",")}" do
+    #insts = @tama.describe_instances($inst_ids)
     
-    # Check if we got the correct number of instances
-    insts.length.should == $res.length
+    ## Check if we got the correct number of instances
+    #insts.length.should == $res.length
     
-    # Check if the instances we got are the correct ones
-    $res.each { |instance|
-      $inst_ids.member?(instance[:aws_instance_id]).should be_true
-    }
-  end
+    ## Check if the instances we got are the correct ones
+    #$res.each { |instance|
+      #$inst_ids.member?(instance[:aws_instance_id]).should be_true
+    #}
+  #end
   
-  $res.each { |inst_map|
-    it "Should describe instance #{inst_map[:aws_instance_id]}" do
-      inst = @tama.describe_instances([inst_map[:aws_instance_id]])
-      inst.length.should == 1
-      inst.first[:aws_instance_id].should == inst_map[:aws_instance_id]
-    end
-  }
+  #$res.each { |inst_map|
+    #it "Should describe instance #{inst_map[:aws_instance_id]}" do
+      #inst = @tama.describe_instances([inst_map[:aws_instance_id]])
+      #inst.length.should == 1
+      #inst.first[:aws_instance_id].should == inst_map[:aws_instance_id]
+    #end
+    
+    #it "Should have security groups #{Security_Groups.join(",")} set for instance #{inst_map[:aws_instance_id]}" do
+      
+    #end
+  #}
   
-  it "Should terminate instances #{$inst_ids.join(",")}" do
-    terms = @tama.terminate_instances($inst_ids)
-    terms.length.should == $inst_ids.length
-    $res.each { |instance|
-      $inst_ids.member?(instance[:aws_instance_id]).should be_true
-    }
-  end
+  #it "Should terminate instances #{$inst_ids.join(",")}" do
+    #terms = @tama.terminate_instances($inst_ids)
+    #terms.length.should == $inst_ids.length
+    #$res.each { |instance|
+      #$inst_ids.member?(instance[:aws_instance_id]).should be_true
+    #}
+  #end
   
-  it "Should describe host nodes" do
-    hosts = @tama.describe_host_nodes
-    hosts.empty?.should be_false
-    hosts.nil?.should be_false
-  end
+  #it "Should describe host nodes" do
+    #hosts = @tama.describe_host_nodes
+    #hosts.empty?.should be_false
+    #hosts.nil?.should be_false
+  #end
   
-  it "Should describe instance specs" do
-    specs = @tama.describe_instance_specs
-    specs.empty?.should be_false
-    specs.nil?.should be_false
-  end
+  #it "Should describe instance specs" do
+    #specs = @tama.describe_instance_specs
+    #specs.empty?.should be_false
+    #specs.nil?.should be_false
+  #end
 end
--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -1,2 +1,85 @@
 require 'rubygems'
 require 'rspec'
+require 'time'
+
+
+shared_examples_for "run_instances" do |result,args|
+  it "Should have started a number of instances between #{args["Min"]} and #{args["Max"]}" do
+    result.length.should >= args["Min"]
+    result.length.should <= args["Max"]
+  end
+end
+
+shared_examples_for "run_instances return values" do |result,args|
+  result.each { |inst_map|
+    it "#{inst_map[:aws_instance_id]} should have a proper time value set for :aws_launch_time" do
+      Time.parse(inst_map[:aws_launch_time]).utc.xmlschema == inst_map[:aws_launch_time]
+    end
+    
+    it "#{inst_map[:aws_instance_id]} should have #{args["Account"]} set as :aws_owner" do
+      inst_map[:aws_owner].should == args["Account"]
+    end
+    
+    it "#{inst_map[:aws_instance_id]} should have #{args["Security_Groups"].keys.inspect} set as :aws_groups" do
+      inst_map[:aws_groups].sort.should == args["Security_Groups"].keys.sort
+    end
+    
+    it "#{inst_map[:aws_instance_id]} should have 0 set as :aws_state_code" do
+      inst_map[:aws_state_code].should == 0
+    end
+    
+    it "#{inst_map[:aws_instance_id]} should have #{args["Image_id"]} set as :aws_image_id" do
+      inst_map[:aws_image_id].should == args["Image_id"]
+    end
+    
+    it "#{inst_map[:aws_instance_id]} should have #{args["Instance_Type"]} set as :aws_instance_type" do
+      inst_map[:aws_instance_type].should == args["Instance_Type"]
+    end
+    
+    #it "#{inst_map[:aws_instance_id]} should have one of #{possible_states.join(",")} set as :aws_state" do
+    
+    #end
+  }
+end
+
+shared_examples_for "describe_instances return values" do |result,args|
+  before(:all) do
+    @uuids = result.map {|i| i[:aws_instance_id]}
+  end
+  
+  #it "Should have described #{result.length} instances" do
+    #result.length.should == result.length
+  #end
+  
+  result.each { |inst_map|
+    #it "#{inst_map[:aws_instance_id]} should have #{args["HOST"]} set as :aws_availability_zone" do
+      #inst_map[:aws_availablity_zone].should == args["HOST"]
+    #end
+    
+    it "#{inst_map[:aws_instance_id]} should have #{args["SSH_Key"]} set as :ssh_key_name" do
+      inst_map[:ssh_key_name].should == args["SSH_Key"]
+    end
+    
+    #it "#{inst_map[:aws_instance_id]} should have #{args[:arch]} set as :architecture" do
+      #inst_map[:architecture].should == args[:arch]
+    #end
+  }
+end
+
+shared_examples_for "terminate_instances" do |tama,uuids|
+  it "should terminate instances #{uuids.join(",")}" do
+    res = tama.terminate_instances(uuids)
+    
+    res.length.should == uuids.length
+    res.map {|i| i[:aws_instance_id]}.sort.should == uuids.sort
+  end
+end
+
+shared_examples_for "describe_host_nodes" do ||
+  
+end
+#shared_examples_for "describe_images" do |result,args|
+  #result.each { |img_map|
+    #it "#{img_map[:aws]}"
+  #}
+#end
